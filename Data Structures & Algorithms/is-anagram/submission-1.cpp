37ae393fb880c2082f7af8a44b930908f5d384ff
Vaibhav Kr. Mishra <79004538+VaibhavKrMishr@users.noreply.github.com>
2026-04-29 12:59:49 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.size()==t.size()){
+            bool An=false;
+            for(int i=0; i<s.size();i++){
+                for (int j = 0; j < s.size() - i - 1; j++) {
+                    if (s[j] > s[j + 1])
+                        swap(s[j], s[j + 1]);
+                    if (t[j] > t[j + 1])
+                        swap(t[j], t[j + 1]);
+                }
+            }
+
+            return s==t;
+         }
+    }
+};
