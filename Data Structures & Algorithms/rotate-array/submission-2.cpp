dc49bf3d13890ece1c786c897199aaab0d0e9852
Vaibhav Kr. Mishra <79004538+VaibhavKrMishr@users.noreply.github.com>
2026-05-10 20:50:24 +0530

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>res;
+        k %= n;
+        for (int i=n-k;i<n;i++){
+            res.push_back(nums[i]);
+        }
+        for(int i=0;i<n-k;i++){
+            res.push_back(nums[i]);
+        }
+        nums=res;
+    }
+};
